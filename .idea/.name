ca6b294c596e166699c42524d071c046308ea0dd
nisioka <nishioka.daisuke@gmail.com>
2016-04-06 08:05:14 +0900

    Initial commit

---
+++ .idea/.name
@@ -0,0 +1,1 @@
+WebViewApplication
---
+++ app/src/androidTest/java/com/sun0range/anyone_blog/webviewapplication/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.sun0range.anyone_blog.webviewapplication;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
